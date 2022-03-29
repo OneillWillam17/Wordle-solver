8f74f623a1c964e259ed083fd6bcf242f414b42c
William O'Neill <99112665+OneillWillam17@users.noreply.github.com>
2022-03-29 17:58:10 -0400

    Add files via upload
    Improved readability, added more comments to explain code and adjusted variable names to be more explicit

--- brain.py
+++ brain.py
@@ -4,13 +4,11 @@ from random import choice
 class Solver:
 
     def __init__(self):
-
         # format word list
         with open('wordle-nyt-answers-alphabetical.txt') as file:
             self.wordlist = [line.strip('\n') for line in file]
 
-        # green letters is a dict to allow us to save index and value
-        self.green_letters = {
+        self.correct_location_and_letters = {
             0: None,
             1: None,
             2: None,
@@ -19,98 +17,73 @@ class Solver:
         }
         self.yellow_letters = []
         self.wrong_letters = []
-
-        # stores any words already guessed by program
-        self.already_guessed = []
-
-        # used in self.guess_word() to display any potential words the letters may show
+        self.already_guessed_words = []
         self.confirmed_letters = []
-
-        # latest guess from self.guess_word()
-        self.guess = None
+        self.latest_guess = None
 
     def save_letters(self):
         """For use in the manual version of this program, prompts the user to enter which letters from the guess are
-        'green' (green letter means it is the correct letter AND its in the correct location in the word"""
-        # before saving new letters remove old ones from list
+        'green' (green letter means it is the correct letter AND its in the correct location in the word) or yellow
+        (meaning correct letter but wrong location)
+        """
+        # remove letters from previous guess
         self.clear_letters()
 
         correct_letters = input("Were there any correct letters, Y or N: ").lower()
-
         if correct_letters == 'n':
             # all letters in the guess were incorrect, meaning they cannot be in the correct word
-            for char in self.guess:
+            for char in self.latest_guess:
                 self.wrong_letters.append(char)
         else:
-            # Green letters
+            # Correct letters in the correct location
             green_input = input('Enter green letters:, leave blank if none: ').lower()
 
-            # if all character are green, meaning we found word and win game
-            if green_input == self.guess:
-                print("Yay we got it!")
+            # if all letters are green, we found word and win game
+            if green_input == self.latest_guess:
+                print("Word solved!")
 
-            for char in self.guess:
+            for char in self.latest_guess:
                 if char in green_input:
                     # character is in both user input and the programs guess
                     # means the character is confirmed in the correct spot within the guess
-                    index = self.guess.index(char) # todo issue here for double letters
-                    self.green_letters[index] = char
+                    index = self.latest_guess.index(char)  # todo issue here for double letters
+                    self.correct_location_and_letters[index] = char
 
             # Yellow letters
             yellow_input = input('Enter yellow letters: leave blank if none: ').lower()
-
             for char in yellow_input:
                 # these letters are confirmed in the word, but not in a guaranteed spot.
                 self.yellow_letters.append(char)
 
-            # add letters from self.guess that weren't correct to wrong_letters
-            for letter in self.guess:
-
-                # letters in self.guess that weren't added to yellow/green list; word cannot contain letter
+            # letters in self.guess that weren't added to yellow/green list; word cannot contain letter
+            for letter in self.latest_guess:
                 if letter not in yellow_input and letter not in green_input:
                     self.wrong_letters.append(letter)
                     print(f"self.wrong_letter added: {letter}")
 
-            # add self.guess to list, so we don't guess same word twice
-            self.already_guessed.append(self.guess)
+            self.already_guessed_words.append(self.latest_guess)
 
     def guess_word(self):
         """Returns a word based on any green letters or yellow letters saved, if neither (ie first guess) returns
         a word from the list at random"""
-
-        for char in self.green_letters.values():
-
-            # print(f"self.green_letters char: {char}")
-
+        for char in self.correct_location_and_letters.values():  # we know letters AND their location within the word
             if char is None:
-                pass  # We don't know the corresponding character for this location
+                pass                                       # We don't know the corresponding character for this location
             else:
                 self.confirmed_letters += char
-
-        if len(self.yellow_letters) > 0:
-            # we know some letters but not their locations within the word
-
+        if len(self.yellow_letters) > 0:                  # we know some letters but not their locations within the word
             for char in self.yellow_letters:
                 self.confirmed_letters += char
 
-        # remove duplicates in potential_word
-        self.confirmed_letters = list(set(self.confirmed_letters))
+        self.confirmed_letters = list(set(self.confirmed_letters))  # remove duplicates within confirmed_letters
+        self.update_wordlist()                                      # removes incorrect words from list
+        self.save_wordlist()                                        # saves possible solutions to txt file
+        self.latest_guess = choice(self.wordlist)                   # picks a word at random from remaining words
 
-        print(f"Potential_word: {self.confirmed_letters}")
+        print(f"Green letter dict: {self.correct_location_and_letters}")
+        print(f"Confirmed_letters: {self.confirmed_letters}")       # displays list of characters found within word
 
-        # removes incorrect words from list
-        self.update_wordlist()
-
-        # saves possible solutions to txt file, mainly testing purposes
-        self.save_wordlist()
-
-        # picks a word at random from remaining words
-        self.guess = choice(self.wordlist)
-
-        # prints the dict, helpful to see which green letter is located where
-        print(f"Green letter dict: {self.green_letters}")
-
-        return self.guess
+        return self.latest_guess
 
     def update_wordlist(self):
         """iterates through self.wordlist removes any words that don't contain letters in
@@ -118,61 +91,53 @@ class Solver:
 
         # using copy of list to not mess up indexing while removing things from list
         for word in self.wordlist[:]:
-
             # remove any words we have already guessed previously
-            for used_word in self.already_guessed[:]:
+            for used_word in self.already_guessed_words[:]:
                 print(f'used_word: {used_word}')
-
-                # so the program doesn't attempt to remove the word several times per loop
                 print(f"Removing word: {word}, reason: self.already_guessed")
-                self.already_guessed.remove(used_word)
-
+                # so the program doesn't attempt to remove the word several times per loop
+                self.already_guessed_words.remove(used_word)
                 self.wordlist.remove(used_word)
 
+            # if word contains a letter we know isn't in the solution, remove word from list
             for char in self.wrong_letters:
-                if char in word:  # word contains a letter we know isn't in the solution, remove word from list
+                if char in word:
                     print(f"Removing word: {word}, reason: self.wrong_letters")
                     self.try_remove_word(word)
 
-            # yellow letters
+            # remove these words since they don't contain a letter we already know is in the word
             for char in self.yellow_letters:
                 if char not in word:
-
-                    # we remove these words since they don't contain a letter we have already confirmed in the word
                     print(f"Removing word: {word}, reason: self.yellow_letters")
                     self.try_remove_word(word)
 
-            # green letters
-            for key, char in self.green_letters.items():
-
-                # checks characters within green_letter dict to see if they are assigned (aka not None)
+            for key, char in self.correct_location_and_letters.items():
+                # checks characters within correct loc and letter dict to see if they are assigned (aka not None)
                 if char is not None:
-                    # means there is a character stored in the dict
-
                     if char not in word:
-                        # we remove these words since they don't contain a letter we have already confirmed in the word
+                        # remove these words since they don't contain a letter we have already confirmed in the word
                         print(f"Removing word: {word}, reason: self.green_letters.items()")
                         self.try_remove_word(word)
 
                     for _ in word:
-                        if word[key] != char:  # The word contains the letter but in the wrong location
+                        if word[key] != char:
+                            # The word contains the letter but in the wrong location
                             print(f"Removing word: {word}, reason: word[key] != char")
                             self.try_remove_word(word)
                         else:
-                            pass  # word contains both the character, and in the correct location
+                            # word contains both the character, and in the correct location
+                            pass
 
     def clear_letters(self):
         """Resets the green and yellow letter lists, called before saving new guess"""
-
-        self.green_letters = {
+        self.yellow_letters = []
+        self.correct_location_and_letters = {
             0: None,
             1: None,
             2: None,
             3: None,
             4: None,
         }
-
-        self.yellow_letters = []
 
     def save_wordlist(self):
         """for testing. saves all remaining words to new text file"""
--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from brain import Solver
 
+# Read the list of potential words from text file, and setup lists for green/yellow letters
 wordle = Solver()
 
-for i in range(5):
+# Guess's word and saves potential solutions to textfile
+# loop repeats 5 times for the 5 free guesses within the game
+# after loop ends (meaning word has not been solved and game is on its last guess) guess one final time
+# only looping 5 times because after program saves data from 5th guess; no benefit to saving new data from 6th guess
+# as the game would end regardless of whether word was correct or not
+for _ in range(5):
+    # write all potential solutions to a text file for debugging / testing purposes
     wordle.save_wordlist()
 
+    # prints random word from list of potential solutions
     print(f"Guess: {wordle.guess_word()}")
 
+    # asks for user input of which characters are green/yellow and saves those characters to a list/dict
+    # filters remaining words based on those letters
     wordle.save_letters()
 
-# final guess
+# final guess of game, outside loop as no reason to save data after called
 print(f"Final Guess: {wordle.guess_word()}")
 
 # todo potential double letters issues, index always chooses closes to front
